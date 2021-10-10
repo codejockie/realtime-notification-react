1d68667ffc3872b4175833025d0dc7a9a521423f
John C. Kennedy <devjckennedy@gmail.com>
2021-10-10 13:52:46 +0100

    Add subscription to get real-time notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
+import { gql, useSubscription } from "@apollo/client"
+import { ToastContainer, toast } from "react-toastify"
+import { useEffect } from "react"
+import "react-toastify/dist/ReactToastify.css"
+
 import "./App.css"
 import { PushNotification } from "./components/PushNotification/PushNotification"
 
+const NEW_NOTIFICATION_SUBSCRIPTION = gql`
+  subscription {
+    newNotification {
+      label
+    }
+  }
+`
+
 function App() {
+  const { data, loading } = useSubscription(NEW_NOTIFICATION_SUBSCRIPTION, {})
+
+  useEffect(() => {
+    toast(data?.newNotification?.label)
+  }, [data])
+
   return (
     <div className="App">
       <PushNotification />
+      <ToastContainer />
     </div>
   )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,13 +5,39 @@ import {
   ApolloClient,
   HttpLink,
   InMemoryCache,
+  split,
 } from "@apollo/client"
+import { getMainDefinition } from "@apollo/client/utilities"
+import { WebSocketLink } from "@apollo/client/link/ws"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000/graphql",
+})
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:5000/subscriptions",
+  options: {
+    reconnect: true,
+  },
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  httpLink
+)
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:5000/graphql" }),
+  link: splitLink,
   cache: new InMemoryCache(),
 })
 
